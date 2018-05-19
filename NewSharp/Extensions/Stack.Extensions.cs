866bb8305afd064f0a172abafbe198b0b00b2db0
Napen123 <napen123@gmail.com>
2018-05-19 12:33:54 -0600

    Add Stack<T> extensions

---
+++ NewSharp/Extensions/Stack.Extensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace NewSharp.Extensions
+{
+    public static class StackExtensions
+    {
+        public static bool TryPeek<T>(this Stack<T> stack, out T item)
+        {
+            try
+            {
+                item = stack.Peek();
+
+                return true;
+            }
+            catch
+            {
+                item = default(T);
+
+                return false;
+            }
+        }
+
+        public static bool TryPop<T>(this Stack<T> stack, out T item)
+        {
+            try
+            {
+                item = stack.Pop();
+
+                return true;
+            }
+            catch
+            {
+                item = default(T);
+
+                return false;
+            }
+        }
+    }
+}
---
+++ NewSharpTesting/Extensions/StackExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using NewSharp.Extensions;
+
+namespace NewSharpTesting.Extensions
+{
+    [TestClass]
+    public class StackExtensions
+    {
+        [TestMethod]
+        public void TryPeekTest()
+        {
+            var stack = new Stack<string>(1);
+            stack.Push("Hello!");
+
+            {
+                Assert.IsTrue(stack.TryPeek(out var message));
+                Assert.AreEqual("Hello!", message);
+            }
+
+            stack.Pop();
+
+            {
+                Assert.IsFalse(stack.TryPeek(out var message));
+                Assert.IsNull(message);
+            }
+        }
+
+        [TestMethod]
+        public void TryPopTest()
+        {
+            var stack = new Stack<string>(1);
+            stack.Push("Hello!");
+
+            {
+                Assert.IsTrue(stack.TryPop(out var message));
+                Assert.AreEqual("Hello!", message);
+            }
+            
+            {
+                Assert.IsFalse(stack.TryPop(out var message));
+                Assert.IsNull(message);
+            }
+        }
+    }
+}
