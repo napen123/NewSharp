a8806440382d7bfbae49f96f46f34bc7ca4b68dc
Napen123 <napen123@gmail.com>
2018-05-20 19:21:15 -0600

    Add IntoString extensions to IntPtr

---
+++ NewSharp/Extensions/IntPtr.Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace NewSharp.Extensions
+{
+    public static class IntPtrExtensions
+    {
+        public static unsafe string IntoString(this IntPtr ptr, Encoding encoding)
+        {
+            if (ptr == IntPtr.Zero)
+                return null;
+
+            var p = (byte*) ptr;
+            var b = (byte*) ptr;
+
+            while (*b != 0)
+                b++;
+
+            return encoding.GetString(p, (int) (b - p));
+        }
+
+        public static unsafe string IntoString(this IntPtr ptr, Encoding encoding, int length)
+        {
+            if (ptr == IntPtr.Zero)
+                return null;
+
+            var p = (byte*) ptr;
+            var b = ((byte*) ptr) + length;
+
+            return encoding.GetString(p, (int) (b - p));
+        }
+    }
+}
---
+++ NewSharpTesting/Extensions/IntPtrExtensionsTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using NewSharp.Extensions;
+
+namespace NewSharpTesting.Extensions
+{
+    [TestClass]
+    public class IntPtrExtensionsTest
+    {
+        [TestMethod]
+        public void ToUtf8StringTest()
+        {
+            {
+                var ptr = Marshal.AllocHGlobal(7);
+                Marshal.Copy(new byte[] {72, 101, 108, 108, 111, 33, 0}, 0, ptr, 7);
+
+                Assert.AreEqual("Hello!", ptr.ToUtf8String());
+
+                Marshal.FreeHGlobal(ptr);
+            }
+
+            {
+                var ptr = Marshal.AllocHGlobal(6);
+                Marshal.Copy(new byte[] { 72, 101, 108, 108, 111, 33 }, 0, ptr, 6);
+
+                Assert.AreEqual("Hello!", ptr.ToUtf8String(6));
+
+                Marshal.FreeHGlobal(ptr);
+            }
+        }
+    }
+}
