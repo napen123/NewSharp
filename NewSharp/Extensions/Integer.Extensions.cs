a87eaafe47465c871ba7ce527dd9bf7674374a5a
Napen123 <napen123@gmail.com>
2018-05-20 19:02:55 -0600

    Add HasFlag extension to integral types

---
+++ NewSharp/Extensions/Integer.Extensions.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace NewSharp.Extensions
+{
+    public static class IntegerExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool HasFlag(this sbyte i, sbyte flag)
+        {
+            return (i & flag) != 0;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool HasFlag(this short i, short flag)
+        {
+            return (i & flag) != 0;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool HasFlag(this int i, int flag)
+        {
+            return (i & flag) != 0;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool HasFlag(this long i, long flag)
+        {
+            return (i & flag) != 0;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool HasFlag(this byte i, byte flag)
+        {
+            return (i & flag) != 0;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool HasFlag(this ushort i, ushort flag)
+        {
+            return (i & flag) != 0;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool HasFlag(this uint i, uint flag)
+        {
+            return (i & flag) != 0;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool HasFlag(this ulong i, ulong flag)
+        {
+            return (i & flag) != 0;
+        }
+    }
+}
---
+++ NewSharpTesting/Extensions/IntegerExtensionsTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using NewSharp.Extensions;
+
+namespace NewSharpTesting.Extensions
+{
+    [TestClass]
+    public class IntegerExtensionsTest
+    {
+        [TestMethod]
+        public void HasFlag()
+        {
+            const int flagA = 1;
+            const int flagB = 2;
+
+            {
+                const int a = flagA;
+
+                Assert.IsTrue(a.HasFlag(flagA));
+                Assert.IsFalse(a.HasFlag(flagB));
+            }
+
+            {
+                const int b = flagB;
+
+                Assert.IsFalse(b.HasFlag(flagA));
+                Assert.IsTrue(b.HasFlag(flagB));
+            }
+
+            {
+                const int ab = flagA | flagB;
+
+                Assert.IsTrue(ab.HasFlag(flagA));
+                Assert.IsTrue(ab.HasFlag(flagB));
+            }
+        }
+    }
+}
