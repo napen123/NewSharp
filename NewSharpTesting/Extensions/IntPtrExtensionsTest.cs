c29e62e916e34905bafa8dd0504d83f2f7473853
Napen123 <napen123@gmail.com>
2018-08-03 15:27:10 -0600

    Update IntPtrExtensionsTest

--- NewSharpTesting/Extensions/IntPtrExtensionsTest.cs
+++ NewSharpTesting/Extensions/IntPtrExtensionsTest.cs
@@ -1,5 +1,6 @@
-﻿using System.Runtime.InteropServices;
-using System.Text;
+﻿using System.Text;
+using System.Runtime.InteropServices;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using NewSharp.Extensions;
@@ -10,22 +11,24 @@ namespace NewSharpTesting.Extensions
     public class IntPtrExtensionsTest
     {
         [TestMethod]
-        public void ToUtf8StringTest()
+        public void IntoManagedStringTest()
         {
+            var hello = new byte[] {72, 101, 108, 108, 111, 33, 0};
+
             {
                 var ptr = Marshal.AllocHGlobal(7);
-                Marshal.Copy(new byte[] {72, 101, 108, 108, 111, 33, 0}, 0, ptr, 7);
+                Marshal.Copy(hello, 0, ptr, 7);
 
-                Assert.AreEqual("Hello!", ptr.IntoString(Encoding.UTF8));
+                Assert.AreEqual("Hello!", ptr.IntoManagedString(Encoding.UTF8));
 
                 Marshal.FreeHGlobal(ptr);
             }
 
             {
                 var ptr = Marshal.AllocHGlobal(6);
-                Marshal.Copy(new byte[] { 72, 101, 108, 108, 111, 33 }, 0, ptr, 6);
+                Marshal.Copy(hello, 0, ptr, 6);
 
-                Assert.AreEqual("Hello!", ptr.IntoString(Encoding.UTF8, 6));
+                Assert.AreEqual("Hello!", ptr.IntoManagedString(Encoding.UTF8, 6));
 
                 Marshal.FreeHGlobal(ptr);
             }
