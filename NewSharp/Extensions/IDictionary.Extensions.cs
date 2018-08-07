c9ff4acc4f2c1f59cd2f5298ea08cc95d0e7f5d9
Napen123 <napen123@gmail.com>
2018-08-06 18:08:22 -0600

    Add IDictionary.GetOrAddDefault

---
+++ NewSharp/Extensions/IDictionary.Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace NewSharp.Extensions
+{
+    public static class IDictionaryExtensions
+    {
+        public static TValue GetOrAddDefault<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key)
+        {
+            if (dict.TryGetValue(key, out var value))
+                return value;
+
+            dict.Add(key, default);
+
+            return default;
+        }
+    }
+}
---
+++ NewSharpTesting/Extensions/IDictionaryExtensionsTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NewSharp.Extensions;
+
+namespace NewSharpTesting.Extensions
+{
+    [TestClass]
+    public class IDictionaryExtensionsTest
+    {
+        [TestMethod]
+        public void GetOrAddDefaultTest()
+        {
+            const string key = "key";
+
+            var dict = new Dictionary<string, int>();
+            Assert.AreEqual(default, dict.GetOrAddDefault(key));
+
+            dict[key] = 5;
+            Assert.AreEqual(5, dict.GetOrAddDefault(key));
+
+            dict.Remove(key);
+            Assert.AreEqual(default, dict.GetOrAddDefault(key));
+        }
+    }
+}
