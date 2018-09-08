bc2c0b11a3077692e5067500ccb3ca44bf85a1ea
Napen123 <napen123@gmail.com>
2018-09-08 17:47:58 -0600

    Update IDictionaryExtensions
    * Rename IDictionaryExtensions to DictionaryExtensions.
    * Add concrete versions of the extensions.

---
+++ NewSharp/Extensions/Dictionary.Extensions.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Collections.Concurrent;
+
+namespace NewSharp.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key, TValue newValue)
+        {
+            if (dict.TryGetValue(key, out var value))
+                return value;
+
+            dict.Add(key, newValue);
+
+            return newValue;
+        }
+
+        public static TValue GetOrAdd<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, TValue newValue)
+        {
+            if (dict.TryGetValue(key, out var value))
+                return value;
+
+            dict.Add(key, newValue);
+
+            return newValue;
+        }
+
+        public static TValue GetOrAddDefault<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key)
+        {
+            if (dict.TryGetValue(key, out var value))
+                return value;
+
+            dict.Add(key, default);
+
+            return default;
+        }
+
+        public static TValue GetOrAddDefault<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key)
+        {
+            if (dict.TryGetValue(key, out var value))
+                return value;
+
+            dict.Add(key, default);
+
+            return default;
+        }
+
+        public static TValue GetOrAddDefault<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> dict, TKey key)
+        {
+            return dict.GetOrAdd(key, default(TValue));
+        }
+    }
+}
