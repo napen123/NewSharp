8d81cc803773d3029995bac3793f1b8231a286bc
Napen123 <napen123@gmail.com>
2018-09-08 17:06:29 -0600

    IntoManagedString -> ToManagedString

--- NewSharpTesting/Extensions/IntPtrExtensionsTest.cs
+++ NewSharpTesting/Extensions/IntPtrExtensionsTest.cs
@@ -19,7 +19,7 @@ namespace NewSharpTesting.Extensions
                 var ptr = Marshal.AllocHGlobal(7);
                 Marshal.Copy(hello, 0, ptr, 7);
 
-                Assert.AreEqual("Hello!", ptr.IntoManagedString(Encoding.UTF8));
+                Assert.AreEqual("Hello!", ptr.ToManagedString(Encoding.UTF8));
 
                 Marshal.FreeHGlobal(ptr);
             }
@@ -28,7 +28,7 @@ namespace NewSharpTesting.Extensions
                 var ptr = Marshal.AllocHGlobal(6);
                 Marshal.Copy(hello, 0, ptr, 6);
 
-                Assert.AreEqual("Hello!", ptr.IntoManagedString(Encoding.UTF8, 6));
+                Assert.AreEqual("Hello!", ptr.ToManagedString(Encoding.UTF8, 6));
 
                 Marshal.FreeHGlobal(ptr);
             }
