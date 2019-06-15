cd50a8245dda25683f5a87ade650a15cae5d20c8
Napen123 <napen123@gmail.com>
2019-06-15 15:22:34 -0600

    Add more overloads for NumberParser.TryParse

--- NewSharp/Numerics/NumberParser.cs
+++ NewSharp/Numerics/NumberParser.cs
@@ -1,4 +1,7 @@
-﻿namespace NewSharp.Numerics
+﻿using System;
+using System.Globalization;
+
+namespace NewSharp.Numerics
 {
     public static class NumberParser
     {
@@ -11,9 +14,23 @@
                 : Option.None<sbyte>();
         }
 
+        public static Option<sbyte> TryParseInt8(this string str, NumberStyles style, IFormatProvider provider)
+        {
+            return sbyte.TryParse(str, style, provider, out var i)
+                ? Option.Some(i)
+                : Option.None<sbyte>();
+        }
+
         public static Option<short> TryParseInt16(this string str)
         {
             return short.TryParse(str, out var i)
+                ? Option.Some(i)
+                : Option.None<short>();
+        }
+
+        public static Option<short> TryParseInt16(this string str, NumberStyles style, IFormatProvider provider)
+        {
+            return short.TryParse(str, style, provider, out var i)
                 ? Option.Some(i)
                 : Option.None<short>();
         }
@@ -25,9 +42,23 @@
                 : Option.None<int>();
         }
 
+        public static Option<int> TryParseInt32(this string str, NumberStyles style, IFormatProvider provider)
+        {
+            return int.TryParse(str, style, provider, out var i)
+                ? Option.Some(i)
+                : Option.None<int>();
+        }
+
         public static Option<long> TryParseInt64(this string str)
         {
             return long.TryParse(str, out var i)
+                ? Option.Some(i)
+                : Option.None<long>();
+        }
+
+        public static Option<long> TryParseInt64(this string str, NumberStyles style, IFormatProvider provider)
+        {
+            return long.TryParse(str, style, provider, out var i)
                 ? Option.Some(i)
                 : Option.None<long>();
         }
@@ -39,9 +70,23 @@
                 : Option.None<byte>();
         }
 
+        public static Option<byte> TryParseUInt8(this string str, NumberStyles style, IFormatProvider provider)
+        {
+            return byte.TryParse(str, style, provider, out var i)
+                ? Option.Some(i)
+                : Option.None<byte>();
+        }
+
         public static Option<ushort> TryParseUInt16(this string str)
         {
             return ushort.TryParse(str, out var i)
+                ? Option.Some(i)
+                : Option.None<ushort>();
+        }
+
+        public static Option<ushort> TryParseUInt16(this string str, NumberStyles style, IFormatProvider provider)
+        {
+            return ushort.TryParse(str, style, provider, out var i)
                 ? Option.Some(i)
                 : Option.None<ushort>();
         }
@@ -53,9 +98,23 @@
                 : Option.None<uint>();
         }
 
+        public static Option<uint> TryParseUInt32(this string str, NumberStyles style, IFormatProvider provider)
+        {
+            return uint.TryParse(str, style, provider, out var i)
+                ? Option.Some(i)
+                : Option.None<uint>();
+        }
+
         public static Option<ulong> TryParseUInt64(this string str)
         {
             return ulong.TryParse(str, out var i)
+                ? Option.Some(i)
+                : Option.None<ulong>();
+        }
+
+        public static Option<ulong> TryParseUInt64(this string str, NumberStyles style, IFormatProvider provider)
+        {
+            return ulong.TryParse(str, style, provider, out var i)
                 ? Option.Some(i)
                 : Option.None<ulong>();
         }
