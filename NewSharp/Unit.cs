59da041918876f2199533148173f70593653db0b
Napen123 <napen123@gmail.com>
2019-06-15 15:44:09 -0600

    Add simple overrides for Unit

--- NewSharp/Unit.cs
+++ NewSharp/Unit.cs
@@ -3,8 +3,63 @@
 namespace NewSharp
 {
     [Serializable]
-    public readonly struct Unit
+    public readonly struct Unit : IEquatable<Unit>, IComparable<Unit>
     {
         public static readonly Unit Empty = new Unit();
+
+        public bool Equals(Unit other)
+        {
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Unit;
+        }
+
+        public int CompareTo(Unit other)
+        {
+            return 0;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return "(unit)";
+        }
+
+        public static bool operator ==(Unit left, Unit right)
+        {
+            return true;
+        }
+
+        public static bool operator !=(Unit left, Unit right)
+        {
+            return false;
+        }
+
+        public static bool operator <(Unit left, Unit right)
+        {
+            return false;
+        }
+
+        public static bool operator >(Unit left, Unit right)
+        {
+            return false;
+        }
+
+        public static bool operator <=(Unit left, Unit right)
+        {
+            return true;
+        }
+
+        public static bool operator >=(Unit left, Unit right)
+        {
+            return true;
+        }
     }
 }
